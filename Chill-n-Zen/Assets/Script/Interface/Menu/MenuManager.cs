73c2ff27c5d9260379adb529f07360c0d21109b8
BVerstaen <bverstaen04@gmail.com>
2024-02-23 18:24:08 +0100

    Create function signature of each buttons
    Modified :
    MainMenu -> assignation
    MenuManager -> create function signature + Quit function

--- Chill-n-Zen/Assets/Script/Interface/Menu/MenuManager.cs
+++ Chill-n-Zen/Assets/Script/Interface/Menu/MenuManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GotoSelectionLevels()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GotoOptions()
     {
-        
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
