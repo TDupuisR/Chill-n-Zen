e3589c0dc5c1931fa281ca0bed40a25516af2851
LucLenne <113776725+LucLenne@users.noreply.github.com>
2024-02-26 11:56:36 +0100

    Select Level Finish

--- Chill-n-Zen/Assets/Script/SelectLevel.cs
+++ Chill-n-Zen/Assets/Script/SelectLevel.cs
@@ -1,33 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectLevel : MonoBehaviour
 {
-    public void SelectLevel1()
+    [SerializeField] private int _numberLevel;
+    public void ChooseLevel()
     {
-        throw new System.NotImplementedException("Selection level not implemented");
+        SceneManager.LoadScene(_numberLevel);
     }
-
-    public void SelectLevel2()
-    {
-        throw new System.NotImplementedException("Selection level not implemented");
-    }
-
-    public void SelectLevel3()
-    {
-        throw new System.NotImplementedException("Selection level not implemented");
-    }
-
-    public void SelectLevel4()
-    {
-        throw new System.NotImplementedException("Selection level not implemented");
-    }
-
-    public void SelectLevel5()
-    {
-        throw new System.NotImplementedException("Selection level not implemented");
-    }
-
-
 }
