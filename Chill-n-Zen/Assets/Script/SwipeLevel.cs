6c580fc1f08b125c07e62e7bc662e89f88ee6ea4
LucLenne <113776725+LucLenne@users.noreply.github.com>
2024-02-26 11:47:39 +0100

    End Selection Level

--- Chill-n-Zen/Assets/Script/SwipeLevel.cs
+++ Chill-n-Zen/Assets/Script/SwipeLevel.cs
@@ -14,12 +14,16 @@ public class SwipeLevel : MonoBehaviour
     [Foldout("Inputs")][SerializeField] InputActionReference _inputPrimaryTouch;
     [Foldout("Inputs")][SerializeField] InputActionReference _inputPrimaryPosition;
     [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private float speedSlider = 1;
     private List<RectTransform> _listPicture = new List<RectTransform>();
     private RectTransform _tempRectTransform = null;
     private float _distance = Mathf.Infinity;
     private float _newXPosition;
     private float _initialOffSet;
     private bool _isDragging;
+    private float posMax;
+    private float posMin;
+
     #endregion
 
     void Start()
@@ -32,38 +36,37 @@ public class SwipeLevel : MonoBehaviour
 
     private void OnEnable()
     {
-        _inputPrimaryTouch.action.started += StartSwipe;
-        _inputPrimaryTouch.action.canceled += EndSwipe;
+        GameplayScript._onSwipe += StartSwipe;
+        GameplayScript._onEndPrimaryTouch += EndSwipe;
     }
 
     private void OnDisable()
     {
-        _inputPrimaryTouch.action.started -= StartSwipe;
-        _inputPrimaryTouch.action.canceled -= EndSwipe;
+        GameplayScript._onSwipe -= StartSwipe;
+        GameplayScript._onSwipe -= EndSwipe;
     }
 
-    private void StartSwipe(InputAction.CallbackContext context)
+    private void StartSwipe(Vector2 velocity)
     {
         _isDragging = true;
-        _initialOffSet = Input.mousePosition.x - _rectTransform.position.x;
-        StartCoroutine(CoroutineRectTransform());
+        StartCoroutine(CoroutineRectTransform(velocity));
     }
 
-    private void EndSwipe(InputAction.CallbackContext context)
+    private void EndSwipe(Vector2 endPosition)
     {
         _isDragging = false;
-        StopCoroutine(CoroutineRectTransform());
+        StopCoroutine(CoroutineRectTransform(endPosition));
         FindClosestImage();
     }
 
 
-    IEnumerator CoroutineRectTransform()
+    IEnumerator CoroutineRectTransform(Vector2 velocity)
     {
         while(_isDragging)
         {
-            _newXPosition = Input.mousePosition.x - _initialOffSet;
-            _newXPosition = Mathf.Clamp(_newXPosition, _listPicture[0].position.x, _listPicture[4].position.x);
-            _rectTransform.position = new Vector3(_newXPosition, _rectTransform.position.y, _rectTransform.position.z);
+            _newXPosition += velocity.x*speedSlider;
+            _newXPosition = Mathf.Clamp(_newXPosition, _listPicture[0].localPosition.x, _listPicture[4].localPosition.x);
+            _rectTransform.localPosition = new Vector3(_newXPosition, 0, 0);
             yield return new WaitForFixedUpdate();
         }
     }
@@ -80,5 +83,6 @@ public class SwipeLevel : MonoBehaviour
             }
         }
         _rectTransform.localPosition = new Vector2(_tempRectTransform.localPosition.x,0);
+        _newXPosition = _rectTransform.localPosition.x;
     }
 }
