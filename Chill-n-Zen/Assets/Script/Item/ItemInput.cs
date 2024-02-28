71fd38f1ecb720a5d79f4c723f8095ee8fb432bf
Nirgigo <titouan@dupuisland.net>
2024-02-28 15:12:20 +0100

    Minor changes, needed for Pull

--- Chill-n-Zen/Assets/Script/Item/ItemInput.cs
+++ Chill-n-Zen/Assets/Script/Item/ItemInput.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemInput : MonoBehaviour
-{   
+{
+    GameplayScript _gameplayScript;
 
+    private void Start()
+    {
+        _gameplayScript = GameplayScript.Instance;
+    }
+
+    private void OnMouseOver()
+    {
+        if ()
+    }
 }
