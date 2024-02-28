8a77597968c62f8105d972a100c47b0fb38229e1
Nirgigo <titouan@dupuisland.net>
2024-02-28 16:03:56 +0100

    ItemInput added
    Add:
    ItemInput.cs

--- Chill-n-Zen/Assets/Script/Item/ItemInput.cs
+++ Chill-n-Zen/Assets/Script/Item/ItemInput.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using GameManagerSpace;
 
 public class ItemInput : MonoBehaviour
 {
-    GameplayScript _gameplayScript;
+    [SerializeField] ItemBehaviour _itemBehave;
+    GameplayScript _gameplay;
+
+    bool _primWasPressed = false;
+    bool _holdWasPressed = false;
 
     private void Start()
     {
-        _gameplayScript = GameplayScript.Instance;
+        _gameplay = GameplayScript.Instance;
     }
 
     private void OnMouseOver()
     {
-        //if () test
+        // Etape 3 -> 2
+        if (CheckIsHolding() && _itemBehave.CurrentState == GMStatic.State.Waiting)
+        {
+            _itemBehave.CurrentState = GMStatic.State.Moving;
+            // UI = false
+        }
+        // Etape 3 -> Rotation
+        if (CheckIsTouching() && _itemBehave.CurrentState == GMStatic.State.Waiting )
+        {
+            _itemBehave.Rotation();
+        }
+        // Etape 4 -> 5
+        if (CheckIsTouching() && _itemBehave.CurrentState == GMStatic.State.Placed)
+        {
+            // UI = true
+        }
+    }
+
+    private void Update()
+    {
+        // Etape 2 -> 3
+        if (_itemBehave.CurrentState == GMStatic.State.Moving && !_gameplay.IsHold)
+        {
+            _itemBehave.CurrentState = GMStatic.State.Waiting;
+            // UI = true
+        }
+
+        _primWasPressed = _gameplay.IsPrimaryPressed;
+        _holdWasPressed = _gameplay.IsHold;
+    }
+
+    private bool CheckIsHolding()
+    {
+        return _gameplay.IsPrimaryPressed && !_gameplay.IsSecondaryPressed && _gameplay.IsHold;
+    }
+    private bool CheckIsTouching()
+    {
+        return _primWasPressed && !_holdWasPressed && !_gameplay.IsSecondaryPressed;
     }
 }
