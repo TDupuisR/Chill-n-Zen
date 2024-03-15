728d3fbeb8b2403c03d2c6917abe791e10d1a6c9
BVerstaen <bverstaen04@gmail.com>
2024-03-15 20:23:25 +0100

    Remove uneccesary event & public ReturnMainMenu test

--- Chill-n-Zen/Assets/Script/SwipeLevel.cs
+++ Chill-n-Zen/Assets/Script/SwipeLevel.cs
@@ -38,7 +38,6 @@ public class SwipeLevel : MonoBehaviour
     {
         GameplayScript.onSwipe += StartSwipe;
         GameplayScript.onEndPrimaryTouch += EndSwipe;
-        _inputBackButton.action.started += ReturnMainMenu;
     }
 
 
@@ -46,8 +45,6 @@ public class SwipeLevel : MonoBehaviour
     {
         GameplayScript.onSwipe -= StartSwipe;
         GameplayScript.onEndPrimaryTouch -= EndSwipe;
-        _inputBackButton.action.started -= ReturnMainMenu;
-
     }
 
     private void StartSwipe(Vector2 velocity)
@@ -63,7 +60,7 @@ public class SwipeLevel : MonoBehaviour
         FindClosestImage();
     }
 
-    private void ReturnMainMenu(InputAction.CallbackContext obj)
+    public void ReturnMainMenu()
     {
         if (!_isLoading)
         {
