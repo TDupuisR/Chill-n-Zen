8489a007c58a65d9193d1609fd80c870a6265465
LucLenne <113776725+LucLenne@users.noreply.github.com>
2024-03-19 19:42:05 +0100

    Success player

--- Chill-n-Zen/Assets/Script/PlayStore/AchievementManager.cs
+++ Chill-n-Zen/Assets/Script/PlayStore/AchievementManager.cs
@@ -6,12 +6,9 @@ using UnityEngine;
 public class AchievementManager : MonoBehaviour
 {
     private bool _check = true;
+    public bool connected = false;
     [SerializeField] Item _cookingPlate;
 
-    public void SetActive(bool active)
-    {
-        this.enabled = active;
-    }
 
     private void OnEnable()
     {
@@ -33,64 +30,73 @@ public class AchievementManager : MonoBehaviour
 
     public void CheckAchievementFloorIsLava()
     {
-
-        GMStatic.requestObj floor = new GMStatic.requestObj();
-        floor.itemRequested = new List<Item>();
-        floor.itemRequested.Add(_cookingPlate);
-        floor.nbRequested = TileSystem.Instance.TilesList.Count-1;
-        if (GameManager.requestManager.CheckObjRequest(floor))
+        if(connected)
         {
-            PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAQ");
+            GMStatic.requestObj floor = new GMStatic.requestObj();
+            floor.itemRequested = new List<Item>();
+            floor.itemRequested.Add(_cookingPlate);
+            floor.nbRequested = TileSystem.Instance.TilesList.Count - 1;
+            if (GameManager.requestManager.CheckObjRequest(floor))
+            {
+                PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAQ");
+            }
         }
     }
     public void CheckAchievementBotanist()
     {
-        GMStatic.requestUsage plante = new GMStatic.requestUsage();
-        plante.usageRequested = new List<GMStatic.tagUsage>();
-        plante.usageRequested.Add(GMStatic.tagUsage.Plant);
-        plante.nbRequested = 10;
-        if (GameManager.requestManager.CheckTypeRequest(plante))
+        if(connected)
         {
-            PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQBA");
+            GMStatic.requestUsage plante = new GMStatic.requestUsage();
+            plante.usageRequested = new List<GMStatic.tagUsage>();
+            plante.usageRequested.Add(GMStatic.tagUsage.Plant);
+            plante.nbRequested = 10;
+            if (GameManager.requestManager.CheckTypeRequest(plante))
+            {
+                PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQBA");
+            }
         }
-        
     }
     public void CheckAchievementGoodStart()
     {
-        if (GameManager.saveData.LoadStar(1) != null)
+        if (GameManager.saveData.LoadStar(1) != null && connected)
         {
             PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQBQ");
         }
     }
     public void CheckAchievementInteriorDesigner()
     {
-        _check = true;
-        for(int i = 1; i <= 10; i++)
+        if (connected)
         {
-            if (GameManager.saveData.LoadStar(i)[2] == false)
+            _check = true;
+            for (int i = 1; i <= 10; i++)
             {
-                _check = false;
+                if (GameManager.saveData.LoadStar(i)[2] == false)
+                {
+                    _check = false;
+                }
             }
-        }
-        if(_check == true)
-        {
-            PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAg");
-        }
-        
+            if (_check == true)
+            {
+                PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAg");
+            }
+        }       
     }
     public void CheckAchievementGraduate()
     {
-        _check = true;
-        for (int i = 1; i <= 10; i++)
+        if (connected)
         {
-            if (GameManager.saveData.LoadStar(i) == null)
+            _check = true;
+            for (int i = 1; i <= 10; i++)
             {
-                _check = false;
+                if (GameManager.saveData.LoadStar(i) == null)
+                {
+                    _check = false;
+                }
             }
-        }
-        if (_check == true)
-        {
-            PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAw");
+            if (_check == true)
+            {
+                PlayGamesPlatform.Instance.UnlockAchievement("CgkI5ZWvkocPEAIQAw");
+            }
         }
     }
 }
